9a7658aae878a9e3f177e5c7fd1450dcf59c6daa
HEVERHD <hever11.hdgd@gmail.com>
2022-05-17 17:47:40 -0500

    x

---
+++ uso_constante.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    define("AUTOR", "Juan");
+
+    echo "La linea de esta instruccion es: " . __LINE__ . "<br>";
+    echo "Estamos trabajando con este fichero" . __FILE__ . "<br>"
+
+    ?>
+</body>
+
+</html>
