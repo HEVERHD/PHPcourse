660e3713dad06e0c79b92c3ba15e3d406d21ec96
HEVERHD <hever11.hdgd@gmail.com>
2022-05-17 12:09:13 -0500

    x

---
+++ dame_nombre.php
@@ -0,0 +1,10 @@
+<?php
+
+
+       
+
+function dameNombre(){
+    $nombre="yerlis";
+}
+
+dameNombre();
---
+++ proporciona_datos.php
@@ -0,0 +1,8 @@
+<?php
+function dameDatos()
+{
+
+echo "este es el mensaje de la funcion <br>";
+}
+
+?>
